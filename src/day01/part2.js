4d63b27759970939f9114bdaff74cac02db323b0
catahache <herediacatalina1995@gmail.com>
2022-12-02 23:25:39 -0300

    part 2

--- src/day01/part2.js
+++ src/day01/part2.js
@@ -1,5 +1,23 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input) {
-  return 0;
+  const calories = input.split(/\r?\n/);
+  const top = [];
+  let elfCalories = 0;
+
+  calories.forEach(calorie => {
+    if(calorie !== ''){
+      elfCalories += parseInt(calorie,10);
+    } else {
+      top.push(elfCalories)
+      elfCalories = 0;
+    }
+  })
+
+  top.sort((a,b) => b - a)
+  const top3 = top.slice(0, 3);
+
+  return top3.reduce((a, b) => {
+    return a + b;
+  }, 0);
 }
