1873432b4ed6b2a992726aed3fe172ddab71c9dd
catahache <herediacatalina1995@gmail.com>
2022-12-02 22:15:02 -0300

    calorie counting

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,18 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+  const calories = input.split(/\r?\n/);
+  let elfCalories = 0;
+  let max = 0;
+  calories.forEach(calorie => {
+    if(calorie !== ''){
+      elfCalories += parseInt(calorie,10);
+    } else {
+      if(elfCalories > max) {
+        max = elfCalories
+      }
+      elfCalories = 0;
+    }
+  })
+  return max;
 }
